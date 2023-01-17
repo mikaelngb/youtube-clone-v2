df9f55071cd9edd342dd7deed546c7ebd1e37ad2
Mikael <mikaelaja10003@gmail.com>
2023-01-18 02:16:36 +0700

    second commit 50:09

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <YouTubeIcon sx={{ color: "red", fontSize: 60 }} />
+      <Typography variant="h5" align="center" mx={1} sx={{ color: "#fff" }}>
+        Youtube V2
+      </Typography>
+    </Link>
+    <SearchBar></SearchBar>
+  </Stack>
+);
+
+export default Navbar;
