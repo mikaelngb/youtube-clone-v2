9c56dc55b93e262d681a89b121d35bc553255645
Mikael <mikaelaja10003@gmail.com>
2023-01-19 01:31:29 +0700

    finish until 1:30:18

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,45 +2,66 @@ import { Box, Stack, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import { SideBar, Videos } from "./";
 
-const Feed = () => (
-  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box
-      sx={{
-        height: { sx: "auto", md: "92vh" },
-        borderRight: "1px solid #3d3d3d",
-        px: { sx: 0, md: 2 },
-      }}
-    >
-      <SideBar></SideBar>
-      <Typography
-        className="copyright"
-        variant="body2"
-        sx={{ mt: 1.5, color: "#fff" }}
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((res) => {
+      setVideos(res.items);
+    });
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
       >
-        Copyright© 2023 Youtube V2
-      </Typography>
-    </Box>
-    <Box
-      p={2}
-      sx={{
-        overflowY: "auto",
-        height: "90vh",
-        flex: 2,
-      }}
-    >
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        New{" "}
-        <span
-          style={{
-            color: "#F31503",
-          }}
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        ></SideBar>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
         >
-          Videos
-        </span>
-      </Typography>
-      <Videos videos={[]}></Videos>
-    </Box>
-  </Stack>
-);
+          Copyright© 2023 Youtube V2
+        </Typography>
+      </Box>
+      <Box
+        p={2}
+        sx={{
+          overflowY: "auto",
+          height: "90vh",
+          flex: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory}{" "}
+          <span
+            style={{
+              color: "#F31503",
+            }}
+          >
+            Videos
+          </span>
+        </Typography>
+        <Videos videos={videos}></Videos>
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
 
-const Videos = () => <div>Videos</div>;
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, i) => (
+        <Box key={i}>
+          {item.id.videoId && <VideoCard video={item}></VideoCard>}
+          {item.id.channelId && (
+            <ChannelCard channelDetail={item}></ChannelCard>
+          )}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
 export default Videos;
